36f45acbd8a548733b40e439f555e12666ff0962
MuffinsThaCat <talzisckind19@gmail.com>
2024-11-05 22:04:20 -0500

    Enarx types

--- src/challenge/types.rs
+++ src/challenge/types.rs
@@ -15,4 +15,30 @@ pub struct ChallengeResult {
     pub verification_ Vec<u8>,
 }
 
+#[derive(Debug)]
+pub enum ChallengeEvidence {
+    AttestationEvidence {
+        attestation_report: AttestationReport,
+        drawbridge_token: DrawbridgeToken,
+        keep_health: KeepHealth,
+    },
+    ExecutionEvidence {
+        result_hash: Vec<u8>,
+        execution_proof: Vec<u8>,
+        keep_measurement: Vec<u8>,
+    },
+}
+
+#[derive(Debug)]
+pub struct Challenge {
+    pub id: u128,
+    pub challenger: Address,
+    pub challenged: Address,
+    pub challenge_type: ChallengeType,
+    pub execution_id: Option<u128>,
+    pub timestamp: SystemTime,
+    pub deadline: SystemTime,
+    pub status: ChallengeStatus,
+}
+
 pub const MIN_VERIFICATION_PROOFS: usize = 3;
